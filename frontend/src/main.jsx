41e87ae60c392a4925686c4aa4c4046268621a3d
IkraDev <s.katlein@gmail.com>
2023-07-27 10:03:15 +0200

    changed to vite from CRA

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import './index.css';
-import reportWebVitals from './reportWebVitals';
 
+import './index.css'
 
 import StringReverser from './Pages/StringReverser';
 
@@ -25,8 +24,3 @@ root.render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
