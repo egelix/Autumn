1636e61199e35cd9f58ec598d23004537fa9c50e
Martin Egert <m.egert82@gmail.com>
2023-08-10 10:54:47 +0200

    Implemented UserController and 2 Endpoints

--- backend/src/main/java/com/example/journey/autumn/controller/UserController.java
+++ backend/src/main/java/com/example/journey/autumn/controller/UserController.java
@@ -2,11 +2,8 @@ package com.example.journey.autumn.controller;
 
 import com.example.journey.autumn.model.User;
 import com.example.journey.autumn.repository.UserRepository;
-import jdk.jshell.Snippet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
