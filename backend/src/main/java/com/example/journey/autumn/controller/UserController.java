d3428e905536f0f7e9b6b465d3ca7ac0ba9ef1b8
Martin Egert <m.egert82@gmail.com>
2023-08-10 10:54:05 +0200

    Implemented UserController and 2 Endpoints

---
+++ backend/src/main/java/com/example/journey/autumn/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.example.journey.autumn.controller;
+
+import com.example.journey.autumn.model.User;
+import com.example.journey.autumn.repository.UserRepository;
+import jdk.jshell.Snippet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    UserRepository userRepository;
+    @GetMapping(value = "/all", produces = "application/json")
+    public List<User> getAllUsers() {
+        List<User> allUsers = userRepository.findAll();
+        System.out.println(allUsers);
+        return allUsers;
+    }
+    @PostMapping(value = "/create", produces = "application/json")
+    public HttpStatus createUser(@RequestBody Map<String, String> requestBody) {
+        userRepository.save(new User(null, requestBody.get("username"), requestBody.get("password")));
+        return HttpStatus.OK;
+    }
+
+
+}
