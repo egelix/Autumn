888c22a7d4748a8d79dcc2f1ba6e34368397dedb
IkraDev <s.katlein@gmail.com>
2023-08-10 12:04:37 +0200

    minor frontend fixes

--- frontend/src/Pages/UserPage.jsx
+++ frontend/src/Pages/UserPage.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const fetchCreateUser = (username, password) => {
-    return fetch("http://localhost:8080/user", {
+    return fetch("http://localhost:8080/user/create", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -11,7 +11,7 @@ const fetchCreateUser = (username, password) => {
 }
 
 const fetchGetAllUsers = () => {
-    return fetch("http://localhost:8080/user").then((res) => res.json());
+    return fetch("http://localhost:8080/user/all").then((res) => res.json());
 }
 
 const fetchDeleteUserById = (id) => {
@@ -28,19 +28,22 @@ const UserPage = () => {
 
     const [users, setUsers] = useState(null);
 
-    const handleUserCreate = (username, password) => {
+    const handleUserCreate = (event, username, password) => {
+        event.preventDefault();
         fetchCreateUser(username, password).then((res) => {
-            print(res);
+            console.log(res);
         })
     }
 
-    const getAllUsers = () => {
+    const getAllUsers = (event) => {
+        event.preventDefault();
         fetchGetAllUsers().then((res) => setUsers(res))
     }
 
-    const deleteUserById = (id) => {
+    const deleteUserById = (event, id) => {
+        event.preventDefault();
         fetchDeleteUserById(id).then((res) => {
-            print(res);
+            console.log(res);
             document.getElementById(id).remove();
         })
     }
@@ -54,8 +57,9 @@ const UserPage = () => {
                 <label>Password:</label>
                 <input type="password" id="passwordInput" />
             </div>
-            <button type="button" onClick={() =>
+            <button type="button" onClick={(e) =>
                 handleUserCreate(
+                    e,
                     document.getElementById("usernameInput").value,
                     document.getElementById("passwordInput").value
                 )}>Create</button>
@@ -65,7 +69,7 @@ const UserPage = () => {
             <ul>
                 {users?.map(user => (
                     <li key={user.id}>{user.username}
-                        <button type="button" onClick={() => deleteUserById(user.id)}>❌</button>
+                        <button type="button" onClick={(e) => deleteUserById(e, user.id)}>❌</button>
                     </li>
                 ))}
             </ul>
