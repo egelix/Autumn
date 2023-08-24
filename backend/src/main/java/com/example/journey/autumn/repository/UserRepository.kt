f21ef572c58e2ee644b2254368ed50b95c4f396a
Martin Egert <m.egert82@gmail.com>
2023-08-24 11:19:33 +0200

    added security with basic authentication

--- backend/src/main/java/com/example/journey/autumn/repository/UserRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.example.journey.autumn.repository
-
-import com.example.journey.autumn.model.User
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface UserRepository : JpaRepository<User?, Int?>
