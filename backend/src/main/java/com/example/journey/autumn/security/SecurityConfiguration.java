a6fed877f40924730868829a5edae5e8e13fc8ec
Martin Egert <m.egert82@gmail.com>
2023-09-16 02:23:11 +0200

    more backend rebuilding

--- backend/src/main/java/com/example/journey/autumn/security/SecurityConfiguration.java
+++ backend/src/main/java/com/example/journey/autumn/security/SecurityConfiguration.java
@@ -1,8 +1,6 @@
 package com.example.journey.autumn.security;
 
-import com.example.journey.autumn.jwt.JwtTokenFilter;
-import com.example.journey.autumn.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,8 +9,11 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -24,29 +25,40 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.List;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfiguration {
-    @Autowired
-    private JwtTokenFilter jwtTokenFilter;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .csrf(csrf -> csrf.disable())
+        http.securityContext(securityContext -> securityContext.requireExplicitSave(false))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                //.csrf().and().cors().disable()
-                .authorizeHttpRequests(auth -> {
-                    /*auth.requestMatchers(HttpMethod.GET, "users").hasAuthority("USER");
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, "users").hasAuthority("USER")
+                        .requestMatchers(HttpMethod.POST, "users").hasAuthority("USER")
+                        .requestMatchers(HttpMethod.GET, "users/{id}").hasAuthority("USER")
+                        .requestMatchers(HttpMethod.DELETE, "users/{id}").hasAuthority("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "users").permitAll()
+                        .anyRequest().authenticated()
+                );
+                http.formLogin(Customizer.withDefaults());
+                http.httpBasic(Customizer.withDefaults());
+                return http.build();
+
+
+                /*.authorizeHttpRequests(auth -> {
+                    *//*auth.requestMatchers(HttpMethod.GET, "users").hasAuthority("USER");
                     auth.requestMatchers(HttpMethod.POST, "users").hasAuthority("USER");
                     auth.requestMatchers(HttpMethod.GET, "users/{id}").hasAuthority("USER");
-                    auth.requestMatchers(HttpMethod.DELETE, "users/{id}").hasAuthority("ADMIN");*/
+                    auth.requestMatchers(HttpMethod.DELETE, "users/{id}").hasAuthority("ADMIN");*//*
                     auth.requestMatchers(HttpMethod.POST, "auth/login").permitAll();
                     auth.requestMatchers(HttpMethod.POST, "users").permitAll();
                     auth.anyRequest().authenticated();
                 })
                 .httpBasic(Customizer.withDefaults())
                 .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
-                .build();
+                .build();*/
     }
 
     private CorsConfigurationSource corsConfigurationSource() {
@@ -54,26 +66,22 @@ public class SecurityConfiguration {
         configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:8080"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
 
-    @Bean
+    /*@Bean
     UserDetailsService userDetailsService(UserRepository userRepository) {
         return username -> userRepository.findByUsername(username)
                 .map(UserPrincipal::new)
                 .orElseThrow(() -> new UsernameNotFoundException(username));
-    }
+    }*/
 
     @Bean
     PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
     }
 
     }
---
+++ backend/src/main/java/com/example/journey/autumn/security/UsernamePwdAuthenticationProvider.java
@@ -0,0 +1,55 @@
+package com.example.journey.autumn.security;
+
+import com.example.journey.autumn.model.Authority;
+import com.example.journey.autumn.model.User;
+import com.example.journey.autumn.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+public class UsernamePwdAuthenticationProvider implements AuthenticationProvider {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        Optional<User> user = userRepository.findByUsername(username);
+        if (!user.isEmpty()) {
+            if (passwordEncoder.matches(password, user.get().getPassword())) {
+                return new UsernamePasswordAuthenticationToken(username, password, getGrantedAuthorities(user.get().getAuthorities()));
+            } else {
+                throw new BadCredentialsException("Invalid password!");
+            }
+        }else {
+            throw new BadCredentialsException("No user registered with this details!");
+        }
+    }
+
+    private List<GrantedAuthority> getGrantedAuthorities(Set<Authority> authorities) {
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        for (Authority authority : authorities) {
+            grantedAuthorities.add(new SimpleGrantedAuthority(authority.getName()));
+        }
+        return grantedAuthorities;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return false;
+    }
+}
