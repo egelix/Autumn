a314a33739730ada98591ba05fe891e65ccbcd6e
KrisHub94 <kristianhuber@hotmail.com>
2023-09-20 14:57:04 +0200

    added info elements

--- frontend/src/components/TutorialCarousel/TutorialCarousel.jsx
+++ frontend/src/components/TutorialCarousel/TutorialCarousel.jsx
@@ -1,23 +1,34 @@
 import { useState } from "react";
+import TUTORIAL_DATA from "./tutorialData";
+import TutorialElement from "./TutorialElement";
 
 const TutorialCarousel = () => {
     const [index, setIndex] = useState(0);
-    const length = 4;
+    const [currentInfoElement, setCurrentInfoElement] = useState(TUTORIAL_DATA[index]);
+    const length = TUTORIAL_DATA.length;
     const handlePrevious = () => {
         const newIndex = index - 1;
         setIndex(newIndex < 0 ? length - 1 : newIndex);
+        setCurrentInfoElement(TUTORIAL_DATA[index]);
       };
     
       const handleNext = () => {
         const newIndex = index + 1;
         setIndex(newIndex >= length ? 0 : newIndex);
+        setCurrentInfoElement(TUTORIAL_DATA[index]);
       };
     return(<div className="tutorial-carousel">
         <div className="tutorial-buttons-header">
             <button onClick={handlePrevious}>PREVIOUS</button>
+            {TUTORIAL_DATA.map((infoElement) => {
+                return <button key={infoElement.index} onClick={() => {
+                    setIndex(infoElement.index);
+                    setCurrentInfoElement(TUTORIAL_DATA[index]);
+                }}>{infoElement.title}</button>
+            })}
             <button onClick={handleNext}>NEXT</button>
         </div>
-        {index}
+        <TutorialElement title={currentInfoElement.title} text={currentInfoElement.text} imgSrc={currentInfoElement.imgSrc} />
     </div>)
 }
 export default TutorialCarousel;
---
+++ frontend/src/components/TutorialCarousel/tutorialData.js
@@ -0,0 +1,21 @@
+const TUTORIAL_DATA = [
+    {
+        title: "Basics",
+        imgSrc: "",
+        text: "Struggle is a very cool game that only the best 1337 pro gamers can play.",
+        index: 0,
+    },
+    {
+        title: "Controls",
+        imgSrc: "",
+        text: "Use the left and right or A and D keys to move and use the space key to jump.",
+        index: 1,
+    },
+    {
+        title: "Coins",
+        imgSrc: "",
+        text: "Gathering a coin will give you +1 score and 1 extra second of time.",
+        index: 2,
+    },
+]
+export default TUTORIAL_DATA;
