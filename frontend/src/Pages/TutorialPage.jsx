cecef6042ea056ba316ff5b75ac1fceae7dd1830
KrisHub94 <kristianhuber@hotmail.com>
2023-09-20 14:38:02 +0200

    created tutorial page

---
+++ frontend/src/Pages/TutorialPage.jsx
@@ -0,0 +1,10 @@
+import TutorialCarousel from "../components/TutorialCarousel/TutorialCarousel"
+
+const TutorialPage = () => {
+    return(<>
+        <h1>HOW TO PLAY</h1>
+        <TutorialCarousel />
+    </>
+    )
+}
+export default TutorialPage;
