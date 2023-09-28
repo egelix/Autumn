62c461158f4592db644ff38b31a61caa00a413c4
KrisHub94 <kristianhuber@hotmail.com>
2023-09-28 12:51:49 +0200

    added credentials check when creating user in frontend

--- frontend/src/components/LoginPage/CreateField.jsx
+++ frontend/src/components/LoginPage/CreateField.jsx
@@ -4,6 +4,7 @@ import BASE_URL from '../../constants';
 export default function CreateField({handleNewUser, setCreateActive}) {
     const [name, setName] = useState("");
     const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
 
     const handleNameChange = (e) => {
         setName(e.target.value);
@@ -13,8 +14,23 @@ export default function CreateField({handleNewUser, setCreateActive}) {
         setPassword(e.target.value);
     }
 
+    const checkCredentials = () => {
+      if(name.length < 5 || name.length > 15) {
+        setError("Username must be between 5 and 15 characters long.");
+        return true;
+      }
+      else if(password.length < 8 || password.length > 20) {
+        setError("Password must be between 8 and 20 characters long.");
+        return true;
+      }
+      return false;
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault();
+        if(checkCredentials()) {
+          return;
+        }
         fetch(BASE_URL + "/users", {
             method: "POST",
             headers: {
@@ -39,6 +55,8 @@ export default function CreateField({handleNewUser, setCreateActive}) {
           <input type='password' name='password'onChange={handlePasswordChange}/>
           <button className='submit-btn'>CREATE!</button>
         </form>
+        {error===null?null
+        :<p className='error-text'>{error}</p>}
     </div>
   )
 }
